f45ca3517f293a16f34e5044b1721cf67a936c99
Hackatack985 <72309893+Hackatack985@users.noreply.github.com>
2020-10-10 22:14:19 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Basic Config (For all bots)
 pythonCommand = "python" # If you have the python command set to something else such as python3
 userToken = ['NzMyMjExNTlyNTExNzAwMDY4.XwxTHg.WJQ9Uej6fmhBDuMTw9LFWO7Gtbo'] # User Tokens here. example 'token','token' | 'email:pass', 'token'
-DiscordChannel = "746398404606951494'' # Channel ID,
+DiscordChannel = "746380702152654848'' # Channel ID,
 SpamSpeed = 0.7 # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
 textRandom = False # if the root has 'text.txt' it will pick a random text line and send it.
 textFull = False # Will send everything written in 'text.txt'
@@ -14,7 +14,7 @@ useBrowser = 'Chrome' # Chrome | Firefox | Edge (windows only) | Set the browser
 joinSpeed = 3 # Changes how fast the bot will start the server joiner
 
 #DM Spam
-DiscordServer = "746380702152654848'' # Server ID (https://a.safe.moe/KP4DR.png this one)
+DiscordServer = "746398404606951494'' # Server ID (https://a.safe.moe/KP4DR.png this one)
 ScanAllServers = False # (True or False) Instead of using a server ID this will scan all of the users joined servers. 
 HeavyScrape = True # (True or False) Use this for servers with a lot of users
 
